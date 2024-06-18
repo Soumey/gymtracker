04866285b605a2a2cee9203b331a52186dbda4fe
Soumey <soumey44@gmail.com>
2024-06-18 22:09:55 +0200

    adding dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+
+require('dotenv').config();
+
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Deck from './models/Deck'
 
 const app = express();
-const PORT = 5000
+const PORT = 5001
 app.use(express.json())// json post requests
 
 
@@ -12,8 +16,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.post('/decks', async (req: Request, res: Response) => {
-    //res.send('Show all decks');
-    console.log(req.body);
     const newDeck = new Deck({
         title: req.body.title
     });
@@ -21,8 +23,9 @@ app.post('/decks', async (req: Request, res: Response) => {
     res.json(createdDeck)
 });
 
-const db = mongoose.connect('mongodb+srv://firstTest:S0ijOGdvWpt5gsxt@firstteast.txfooum.mongodb.net/?retryWrites=true&w=majority&appName=FirstTeast').then(() => {
-    console.log(`listening on ${PORT}`);
-    app.listen(PORT);
-})
-//S0ijOGdvWpt5gsxt
+const db = mongoose.connect(
+    process.env.MONGO_URL!)
+    .then(() => {
+        console.log(`listening on ${PORT}`);
+        app.listen(PORT);
+    })
